e267ed63eb84a96d2f93eb872093176f44ac1fbe
Iamadeyemimuaz <106974751+Adeyemi-Muaz-Ayomide@users.noreply.github.com>
2025-09-30 00:21:01 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import Testimonials from './components/Testimonials';
 import WhyChooseUs from './components/WhyChooseUs';
 import FAQ from './components/FAQ';
 import Blog from './components/Blog';
-import Contact from './components/Contact';
 import Footer from './components/Footer';
 
 function App() {
@@ -22,7 +21,6 @@ function App() {
       <Pricing />
       <Testimonials />
       <FAQ />
-      <Contact />
       <Footer />
     </div>
   );
